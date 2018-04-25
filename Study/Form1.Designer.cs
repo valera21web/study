8bf37b69644f457c9914c40807221359c914f418
Valerii.Shtuvbeinyi <Valerii.Shtuvbeinyi@pricot.com.pl>
2018-04-25 15:09:34 +0200

    init

---
+++ Study/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Study
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.textBoxIV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSaveEnc = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxEnc2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 248);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(636, 434);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(656, 248);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(711, 530);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Encode data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(634, 115);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(12, 205);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(296, 22);
+            this.textBoxKey.TabIndex = 5;
+            // 
+            // textBoxIV
+            // 
+            this.textBoxIV.Location = new System.Drawing.Point(350, 205);
+            this.textBoxIV.Name = "textBoxIV";
+            this.textBoxIV.Size = new System.Drawing.Size(297, 22);
+            this.textBoxIV.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "AES Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "IV";
+            // 
+            // buttonSaveEnc
+            // 
+            this.buttonSaveEnc.Location = new System.Drawing.Point(324, 12);
+            this.buttonSaveEnc.Name = "buttonSaveEnc";
+            this.buttonSaveEnc.Size = new System.Drawing.Size(173, 34);
+            this.buttonSaveEnc.TabIndex = 10;
+            this.buttonSaveEnc.Text = "Save Image";
+            this.buttonSaveEnc.UseVisualStyleBackColor = true;
+            this.buttonSaveEnc.Click += new System.EventHandler(this.buttonSaveEnc_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(656, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 33);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Decode data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxEnc2
+            // 
+            this.textBoxEnc2.Location = new System.Drawing.Point(653, 67);
+            this.textBoxEnc2.Multiline = true;
+            this.textBoxEnc2.Name = "textBoxEnc2";
+            this.textBoxEnc2.Size = new System.Drawing.Size(634, 115);
+            this.textBoxEnc2.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1379, 790);
+            this.Controls.Add(this.textBoxEnc2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonSaveEnc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIV);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.TextBox textBoxIV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSaveEnc;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxEnc2;
+    }
+}
+
---
+++ Study/Form1.cs
@@ -0,0 +1,83 @@
+﻿using LibConvert;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Study
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap imageOriginal;
+        private Bitmap imageEncrypted;
+        private Aes AesKeys = Aes.Create();
+
+        private int countBitsToWrite = 4;
+        private bool R_Available = true;
+        private bool G_Available = true;
+        private bool B_Available = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            AesKeys.Padding = PaddingMode.PKCS7;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Open Image";
+            openFileDialog1.Filter = "Image (*.png, *.jpg)|*.png; *.jpg";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var upload = new Bitmap(openFileDialog1.FileName);
+                imageOriginal = new Bitmap(upload.Width, upload.Height, PixelFormat.Format32bppPArgb);
+                using (Graphics gr = Graphics.FromImage(imageOriginal)) {
+                    gr.DrawImage(upload, new Rectangle(0, 0, imageOriginal.Width, imageOriginal.Height));
+                }
+                pictureBox1.Image = imageOriginal;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxKey.Text = Convert.ToBase64String(AesKeys.Key);
+            //textBoxKey2.Text = Convert.ToBase64String(AesKeys.Key);
+            textBoxIV.Text = Convert.ToBase64String(AesKeys.IV);
+            //textBoxIV2.Text = Convert.ToBase64String(AesKeys.IV);
+
+            var obj = new LibConverter(imageOriginal)
+                .SetR(R_Available)
+                .SetG(G_Available)
+                .SetB(B_Available);
+            obj = obj.SetDataToEncrypt(textBox1.Text);
+            //obj = obj.AESEncryptData(AesKeys.Key, AesKeys.IV);
+            
+            imageEncrypted = new Bitmap(obj.Do());
+            pictureBox2.Image = imageEncrypted;
+        }
+
+        private void buttonSaveEnc_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Png Image (.png)|*.png";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                imageEncrypted.Save(saveFileDialog1.FileName, ImageFormat.Png);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var obj = new LibConverter(imageOriginal).SetEncodedImage(imageEncrypted);
+            var a = obj.Decrypt(AesKeys.Key, AesKeys.IV);
+            textBoxEnc2.Text = a;
+        }
+    }
+}
